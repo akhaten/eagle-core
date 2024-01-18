9cfa5551e6956bd779f7cf5f264c388635cf63ff
akhaten <khafifjessy.github@gmail.com>
2024-01-18 14:18:46 +0100

    default.nix updated

--- default.nix
+++ default.nix
@@ -1,9 +1,7 @@
 
-{ system ? builtins.currentSystem } :
+{ pkgs ? import <nixpkgs> {} }:
 
-let pkgs = import <nixpkgs> { inherit system; };
-
-in pkgs.python311Packages.buildPythonPackage {
+pkgs.python311Packages.buildPythonPackage {
 	name = "eaglecore";
 	version = "0.1";
 	src = ./.;
@@ -12,8 +10,6 @@ in pkgs.python311Packages.buildPythonPackage {
         python311Packages.numpy
         python311Packages.scipy
         python311Packages.matplotlib
-        # python310Packages.pandas
-        # python310Packages.tqdm
 	];
 	doCheck = false;
 }
